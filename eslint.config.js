bef24b48d35494eec2fdf9fce8d563086314bf5f
mfurniel <mfurnielc@gmail.com>
2025-03-04 11:55:16 -0300

    Clean default code and configure linters

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,11 @@ export default [
   ...eslintPluginAstro.configs.recommended,
   prettier,
   {
+    files: ["src/**/*.{js,ts,astro}"],
     rules: {
       "astro/no-unused-define-vars-in-style": "error",
       semi: ["error", "always"],
       "no-unused-vars": ["warn"],
-    },
-  },
-  {
-    files: ["**/*.d.ts"],
-    rules: {
-      "no-unused-vars": "off",
     },
   },
 ];
