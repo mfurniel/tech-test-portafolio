348458c4bb4ef3fcecf45badbfc32bdd95319b79
mfurniel <mfurnielc@gmail.com>
2025-03-04 01:06:10 -0300

    Set up Prettier and ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
+import prettier from "eslint-plugin-prettier/recommended";
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  prettier,
   {
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
       "astro/no-unused-define-vars-in-style": "error",
-      "semi": ["error", "always"],
-      "no-unused-vars": ["warn"]
+      semi: ["error", "always"],
+      "no-unused-vars": ["warn"],
     },
   },
   {
-    // Ignored rules in archives .d.ts
     files: ["**/*.d.ts"],
     rules: {
-      "no-unused-vars": "off"
-    }
-  }
+      "no-unused-vars": "off",
+    },
+  },
 ];
-
