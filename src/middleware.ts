a1cebe643b765d0a7581437f5866512b431bca4e
mfurniel <mfurnielc@gmail.com>
2025-03-07 05:53:09 -0300

    fix: middleware doesn't block image requests now

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import { defineMiddleware } from "astro:middleware";
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
 
+  if (url.pathname.startsWith("/_image")) {
+    return next();
+  }
+
   if (url.pathname !== "/") {
     return new Response(null, {
       status: 302,
