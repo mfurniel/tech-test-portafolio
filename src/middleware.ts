84b2fe1153da91ddde77788bad2b757bdab667d9
mfurniel <mfurnielc@gmail.com>
2025-03-07 16:24:37 -0300

    fix(middleware, accessibility): resolve robots.txt access and improve link accessibility

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import { defineMiddleware } from "astro:middleware";
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
 
-  if (url.pathname.startsWith("/_image")) {
+  // Excluir rutas importantes
+  const excludedPaths = ["/robots.txt", "/_image"];
+
+  if (excludedPaths.some((path) => url.pathname.startsWith(path))) {
     return next();
   }
 
