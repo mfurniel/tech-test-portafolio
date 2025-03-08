73975fb09bb696f3a99ea7fef9f39482dadef673
mfurniel <mfurnielc@gmail.com>
2025-03-08 00:28:56 -0300

    refactor: arrange page beafore of the tests

--- tests/components/layouts/layout.spec.ts
+++ tests/components/layouts/layout.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Layout", () => {
-  test("should have the correct title", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("/");
+  });
+
+  test("should have the correct title", async ({ page }) => {
     await expect(page).toHaveTitle("Tech Test Portafolio");
   });
 
   test("should apply the correct brackgroun color", async ({ page }) => {
-    await page.goto("/");
     await expect(page.locator("body")).toHaveClass("bg-bgOficial");
   });
 
   test("should apply the correct padding and margin", async ({ page }) => {
-    await page.goto("/");
     await expect(page.locator("main")).toHaveClass("p-8 lg:ml-28");
   });
 });
