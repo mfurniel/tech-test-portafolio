d306c8901c79c5d181d503a8f66d3d28bac25451
mfurniel <mfurnielc@gmail.com>
2025-03-07 23:41:33 -0300

    test: added tests for projects

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { defineMiddleware } from "astro:middleware";
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
 
-  // Excluir rutas importantes
   const excludedPaths = ["/robots.txt", "/_image"];
 
   if (excludedPaths.some((path) => url.pathname.startsWith(path))) {
