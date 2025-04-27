a336784466903aed2233b2d9236007bcc393969a
mfurniel <mfurnielc@gmail.com>
2025-04-27 00:20:10 -0400

    feat(darkmode): toggle button update for dark mode

--- src/layouts/ui/ToggleButton.vue
+++ src/layouts/ui/ToggleButton.vue
@@ -1,18 +1,22 @@
 <template>
   <button
     @click="toggle"
-    class="fixed top-4 z-50 border border-gray-400 bg-white p-2 transition-all duration-300 ease-in-out lg:hidden"
+    class="fixed top-4 z-50 border border-gray-400 bg-white p-2 transition-all duration-300 ease-in-out lg:hidden dark:bg-bg-oficial dark:border-gray-700"
     :class="isOpen ? 'left-32' : 'left-4'"
   >
-    <img src="icons/menu.svg" alt="Menú" class="h-4 w-4" />
+    <MenuIcon class="h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8 dark:text-white" />
   </button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import MenuIcon from "../../assets/icons/MenuIcon.vue";
 
 export default defineComponent({
   name: "ToggleButton",
+  components: {
+    MenuIcon,
+  },
   props: {
     isOpen: {
       type: Boolean,
